809ca135b874b7a2b6124ab86dfe1218806eb148
yugonghou <1349726415@qq.com>
2021-07-26 22:03:04 +0800

    test

---
+++ test 收集/test_0612_ vue组件/02-组件之间的通讯/ConfirmModal.vue
@@ -0,0 +1,39 @@
+
+<template>
+<!-- 
+// 操作确认框
+// 调用  <ConfirmModal sureMsg="确认删除吗？" @func="方法名" btnType="1"></ConfirmModal>
+// 按钮类别 ： 1-删除 -->
+  <div class="sureOp">
+    <!-- 删除 -->
+    <Button @click="showOPModal=true" type="error" v-if="btnType=='1'">删除</Button>
+    <Modal v-model="showOPModal" @on-cancel="showOPModal=false" @on-ok="sureOP" title="操作提醒">
+      <h3>{{sureMsg}}</h3>
+    </Modal>
+  </div>
+</template>
+<script>
+export default {
+  name: "confirmModal",
+  props: {
+    sureMsg: {
+      type: String,
+      default: ""
+    },
+    btnType: {
+      type: String,
+      default: "1"
+    }
+  },
+  data() {
+    return {
+      showOPModal: false
+    };
+  },
+  methods: {
+    sureOP() {
+      this.$emit("func");
+    }
+  }
+};
+</script>
---
+++ test 收集/test_0612_ vue组件/03-vue 组件传值/father.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+      <t-button @test="handleClick"></t-button>  
+    </div>
+</template>
+<script>
+import TButton from './Button.vue'
+export default {
+    comments: {
+        TButton
+    },
+    methods: {
+        handleClick () {
+            console.log(333)
+        }
+    }
+}
+</script>
+<style lang="less">
+    
+</style>
